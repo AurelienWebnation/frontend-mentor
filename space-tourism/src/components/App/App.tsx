433dfbef75405012d9e1aa951de717906abb5f4f
Aurélien <aurelien.cabirol@webnation.tech>
2024-04-15 21:03:13 +0200

    chore: remove default export

--- space-tourism/src/components/App/App.tsx
+++ space-tourism/src/components/App/App.tsx
@@ -1,7 +1,5 @@
-import * as React from 'react';
+import { Header } from '../Header';
 
-function App() {
-  return <div></div>;
+export function App() {
+  return <Header />;
 }
-
-export default App;
