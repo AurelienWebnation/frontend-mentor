399c5703a6b59e1916a18f50bf7d9dd16cc8b188
Aurélien <aurelien.cabirol@webnation.tech>
2024-06-12 16:01:17 +0200

    style: global styles

--- body-mass-index-calculator/src/components/GlobalStyle/GlobalStyle.tsx
+++ body-mass-index-calculator/src/components/GlobalStyle/GlobalStyle.tsx
@@ -3,80 +3,99 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
   /* RESET */
   *, *::before, *::after {
-      margin: 0;
-      padding: 0;
-      border: 0;
-      font-size: 100%;
-      font: inherit;
-      vertical-align: baseline;
-      box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+    border: 0;
+    font-size: 100%;
+    font: inherit;
+    vertical-align: baseline;
+    box-sizing: border-box;
   }
-
+  
   article, aside, details, figcaption, figure,
   footer, header, hgroup, menu, nav, section {
-      display: block;
+    display: block;
   }
-
+  
   body {
-      line-height: 1;
-      -webkit-font-smoothing: antialiased;
+    line-height: 1;
+    -webkit-font-smoothing: antialiased;
   }
-
+  
   ol, ul {
-      list-style: none;
+    list-style: none;
   }
-
+  
   blockquote, q {
-      quotes: none;
+    quotes: none;
   }
-
+  
   blockquote:before, blockquote:after,
   q:before, q:after {
-      content: '';
-      content: none;
+    content: '';
+    content: none;
   }
-
+  
   table {
-      border-collapse: collapse;
-      border-spacing: 0;
+    border-collapse: collapse;
+    border-spacing: 0;
   }
-
+  
   img, picture, video, canvas, svg {
-      display: block;
+    display: block;
   }
-
+  
   input, button, textarea, select {
-      font: inherit;
+    font: inherit;
   }
-
+  
   p, h1, h2, h3, h4, h5, h6 {
-      overflow-wrap: break-word;
+    overflow-wrap: break-word;
   }
-
+  
   a {
-      text-decoration: none;
-      color: inherit;
+    text-decoration: none;
+    color: inherit;
   }
-
+  
   #root, #__next {
-      isolation: isolate;
+    isolation: isolate;
   }
-
+  
   html, body, #root {
-      height: 100%;
+    height: 100%;
   }
-
+  
   button {
-      background: transparent;
+    background: transparent;
   }
-
+  
   /* GLOBAL */
   // Fonts
   @font-face {
-      font-family: 'Inter';
-      font-style: normal;
-      font-weight: 400 600;
-      font-display: swap;
-      src: url('/fonts/Inter-VariableFont_slnt,wght.ttf') format('truetype');
+    font-family: 'Inter';
+    font-style: normal;
+    font-weight: 400 600;
+    font-display: swap;
+    src: url('/fonts/Inter-VariableFont_slnt,wght.ttf') format('truetype');
+  }
+  
+  :root {
+    // Colors
+    --color-blue: hsl(227, 92%, 58%);
+    --color-gunmetal: hsl(215, 31%, 21%);
+    --color-dark-electric-blue: hsl(215, 17%, 45%);
+    --color-pure-white: hsl(0, 0%, 100%);
+    
+    // Fonts sizes
+    --font-size-body-m: ${16 / 16}rem;
+    --font-size-body-s: ${14 / 16}rem;
+  }
+  
+  body {
+    font-family: 'Inter', sans-serif;
+    font-size: var(--font-size-body-m);
+    line-height: 150%;
+    color: var(--dark-electric-blue);
   }
 `;
---
+++ body-mass-index-calculator/src/components/Heading/Heading.tsx
@@ -0,0 +1,76 @@
+import { PropsWithChildren } from 'react';
+import styled, { css } from 'styled-components';
+import { QUERIES } from '../../constants.ts';
+
+type Props = PropsWithChildren<{ $size: 'xl' | 'l' | 'm' | 's'; as?: string }>;
+
+const LEVELS = {
+  xl: 1,
+  l: 2,
+  m: 3,
+  s: 4,
+};
+
+export function Heading({
+  $size,
+  as = `h${LEVELS[$size]}`,
+  children,
+  ...delegated
+}: Props) {
+  return (
+    <Wrapper $size={$size} as={as} {...delegated}>
+      {children}
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.h1<Pick<Props, '$size'>>`
+  ${({ $size }) => SIZES_STYLES[$size]};
+`;
+
+const base_styles = css`
+  color: var(--color-gunmetal);
+  font-weight: 600;
+  line-height: 110%;
+`;
+
+const heading_xl_styles = css`
+  ${base_styles};
+  font-size: ${48 / 16}rem;
+  letter-spacing: -2.4px;
+
+  @media ${QUERIES.tabletAndUp} {
+    font-size: ${64 / 16}rem;
+    letter-spacing: -3.2px;
+  }
+`;
+
+const heading_l_styles = css`
+  ${base_styles};
+  font-size: ${32 / 16}rem;
+  letter-spacing: -1.6px;
+
+  @media ${QUERIES.tabletAndUp} {
+    font-size: ${48 / 16}rem;
+    letter-spacing: -2.4px;
+  }
+`;
+
+const heading_m_styles = css`
+  ${base_styles};
+  font-size: ${24 / 16}rem;
+  letter-spacing: -1.2px;
+`;
+
+const heading_s_styles = css`
+  ${base_styles};
+  font-size: ${20 / 16}rem;
+  letter-spacing: -1px;
+`;
+
+const SIZES_STYLES = {
+  xl: heading_xl_styles,
+  l: heading_l_styles,
+  m: heading_m_styles,
+  s: heading_s_styles,
+};
