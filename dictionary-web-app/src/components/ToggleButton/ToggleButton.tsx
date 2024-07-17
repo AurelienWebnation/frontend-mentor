f8c6d2534efaf9f2ee91fa9ec1eda91d3c46a2a9
Aurélien <aurelien.cabirol@webnation.tech>
2024-07-17 15:57:10 +0200

    Implement Toggle dark theme + Toggle button components

---
+++ dictionary-web-app/src/components/ToggleButton/ToggleButton.tsx
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+import { type ChangeEvent, useId } from 'react';
+
+interface Props {
+  handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  checked: boolean;
+}
+
+export function ToggleButton({ handleChange, checked }: Props) {
+  const checkboxId = useId();
+
+  return (
+    <Label htmlFor={checkboxId}>
+      <Checkbox id={checkboxId} checked={checked} onChange={handleChange} />
+    </Label>
+  );
+}
+
+const Label = styled.label`
+  --label-width: 40px;
+
+  background: var(--color-grey-1);
+  display: inline-block;
+  width: var(--label-width);
+  height: 20px;
+  border-radius: 10px;
+  position: relative;
+
+  &:has(input:checked) {
+    background: var(--color-purple);
+  }
+`;
+
+const Checkbox = styled.input.attrs({ type: 'checkbox' })`
+  --checkbox-width: 14px;
+  --padding: 3px;
+
+  appearance: none;
+  width: 14px;
+  height: 14px;
+  background: var(--color-white);
+  border-radius: 50%;
+  transform: translateX(3px) translateY(3px);
+  transition: transform 300ms ease-in-out;
+
+  &:checked {
+    transform: translateX(
+        calc(var(--label-width) - var(--checkbox-width) - var(--padding))
+      )
+      translateY(var(--padding));
+  }
+`;
---
+++ dictionary-web-app/src/components/ToggleDarkTheme/ToggleDarkThemeButton.tsx
@@ -0,0 +1,38 @@
+import { ToggleButton } from '../ToggleButton';
+import styled from 'styled-components';
+import MoonIconSVG from './assets/icon-moon.svg';
+import { useContext } from 'react';
+import { DarkThemeContext } from '../DarkThemeProvider';
+
+export function ToggleDarkTheme() {
+  const { theme, handleDarkThemeChange } = useContext(DarkThemeContext);
+
+  return (
+    <Wrapper>
+      <ToggleButton
+        handleChange={handleDarkThemeChange}
+        checked={theme === 'dark'}
+      />
+      <MoonIcon />
+    </Wrapper>
+  );
+}
+
+const MoonIcon = styled(MoonIconSVG)`
+  width: 20px;
+  height: 20px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
+
+  &:has(input:checked) {
+    ${MoonIcon} {
+      & > path {
+        stroke: var(--color-purple);
+      }
+    }
+  }
+`;
