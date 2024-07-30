e6abc2a6c6c127d996c8ab982938766d08c3506a
Aurélien <aurelien.cabirol@webnation.tech>
2024-07-30 17:00:08 +0200

    Full structure

--- dictionary-web-app/src/components/App/App.tsx
+++ dictionary-web-app/src/components/App/App.tsx
@@ -1,3 +1,23 @@
+import { DarkThemeProvider } from '../DarkThemeProvider';
+import styled from 'styled-components';
+import { Container } from '../Container';
+import { Header } from '../Header';
+import { Dictionary } from '../Dictionary';
+
 export function App() {
-  return <div></div>;
+  return (
+    <DarkThemeProvider>
+      <Wrapper>
+        <Container>
+          <Header />
+          <Dictionary />
+        </Container>
+      </Wrapper>
+    </DarkThemeProvider>
+  );
 }
+
+const Wrapper = styled.div`
+  padding-left: 24px;
+  padding-right: 24px;
+`;
